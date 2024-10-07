c9571960e0f8819bb0d2e7fe6308e38b310ecfdc
Alumnos <agustingabrielescobarv@gmail.com>
2024-10-07 19:29:29 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(160, 352)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Leer"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(358, 352)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Insertar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(68, 137)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(51, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Nombre"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(68, 169)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(68, 80)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(68, 48)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(46, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Codigo"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,68 @@
+﻿
+
+
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+
+    Dim connetionString As String = "Data Source=L201-EQUIPO08\SQLEXPRESS;Initial Catalog=veterinaria;User ID=sa;Password=alumnos"
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Dim cnn As SqlConnection
+        Dim cmd As SqlCommand
+        Dim sql As String
+        Dim reader As SqlDataReader
+
+        sql = "SELECT * FROM Mascota"
+
+        cnn = New SqlConnection(connetionString)
+        Try
+            cnn.Open()
+            cmd = New SqlCommand(sql, cnn)
+            reader = cmd.ExecuteReader()
+            While reader.Read()
+                MsgBox(reader.Item(0) & "  Codigo " & reader.Item(1) & "  Nombre  ")
+            End While
+            reader.Close()
+            cmd.Dispose()
+            cnn.Close()
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim cnn As SqlConnection
+        Dim cmd As SqlCommand
+        Dim sql As String
+
+
+        sql = "INSERT INTO Mascota VALUES('5','Paracas')"
+
+        cnn = New SqlConnection(connetionString)
+        Try
+            cnn.Open()
+            cmd = New SqlCommand(sql, cnn)
+            cmd.ExecuteNonQuery()
+            cmd.Dispose()
+            cnn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+End Class
